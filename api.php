e0a4164d4060d8b530afeade4ff1bda6dbad94d6
diewland <diewland@gmail.com>
2021-04-11 20:34:59 +0700

    read/write done

--- api.php
+++ api.php
@@ -1,13 +1,44 @@
 <?php
   header('Content-Type: application/json');
-  echo json_encode(
-    array(
-      'int' => 99,
-      'float' => 765.433,
-      's' => 'helloworld',
-      'bool1' => true,
-      'bool2' => false,
-    )
-  );
+
+  // get variables from request
+  $filename = $_GET['filename'] ?: 'default.json';
+  $json_str = file_get_contents('php://input');
+
+  // validate file extension
+  if (pathinfo($filename)['extension'] != 'json') {
+    $result = array('err' => "support json file only");
+    echo json_encode($result);
+    exit();
+  }
+
+  // prepare data path
+  $data_path = "./data/$filename";
+
+  // api#1 write data
+  // [POST] /api.php?filename=default.json
+  if ($json_str) {
+
+    $fp = fopen($data_path, 'w');
+    fwrite($fp, $json_str);
+    fclose($fp);
+
+    $result = array('data' => json_decode($json_str));
+    echo json_encode($result);
+  }
+
+  // api#2 read data
+  // [GET] /api.php?filename=default.json
+  else {
+    $result = array();
+    if (file_exists($data_path)) {
+      $result['data'] = json_decode(file_get_contents($data_path));
+    }
+    else {
+      $result['err'] = "$filename not found";
+    }
+    echo json_encode($result);
+  }
+
   exit;
 ?>
